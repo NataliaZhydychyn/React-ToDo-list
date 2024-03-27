3ee91b3357762a579bf05a799669721166787607
Nataliia Zhydychyn <nataliia.zhydychyn@gmail.com>
2024-03-27 21:10:25 +0100

    Persisting the Redux store

--- my-todo-list-app/src/index.js
+++ my-todo-list-app/src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import { configureStore } from './store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { persistStore } from 'redux-persist';
+
+const store = configureStore();
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={configureStore()}>
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
